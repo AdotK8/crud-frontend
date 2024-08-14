a0650289e7777180db30966a0a6fddc24644edb5
Ahmed <ahmedkhan895.ak@gmail.com>
2024-08-14 16:20:25 +0100

    filters for database added

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/table.module.scss";
 import DevelopmentForm from "./Form";
-import { createDevelopment } from "../utils/api";
-import { fetchAllDevelopments } from "../utils/api";
+import { createDevelopment, fetchAllDevelopments } from "../utils/api";
 import { useNavigate } from "react-router-dom";
 
 export default function DevelopmentsTable({ data, setData, setLoading }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [filters, setFilters] = useState({
+    parking: "All",
+    zone: "All",
+    cardinal: "All",
+    oneBed: "All",
+    twoBed: "All",
+    threeBed: "All",
+    fourBed: "All",
+  });
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    refreshData();
+  }, [filters]); // Re-fetch when filters change
 
   const formatNumber = (number) => {
     return new Intl.NumberFormat().format(number);
@@ -26,9 +39,31 @@ export default function DevelopmentsTable({ data, setData, setLoading }) {
   const refreshData = async () => {
     try {
       const result = await fetchAllDevelopments();
-      setData(result);
+      const filteredData = result.filter((development) => {
+        return (
+          (filters.parking === "All" ||
+            development.parking === (filters.parking === "Yes")) &&
+          (filters.zone === "All" ||
+            development.zone === parseInt(filters.zone)) &&
+          (filters.cardinal === "All" ||
+            development.cardinalLocation === filters.cardinal) &&
+          (filters.oneBed === "All" ||
+            development.availability.oneBed.available ===
+              (filters.oneBed === "Yes")) &&
+          (filters.twoBed === "All" ||
+            development.availability.twoBed.available ===
+              (filters.twoBed === "Yes")) &&
+          (filters.threeBed === "All" ||
+            development.availability.threeBed.available ===
+              (filters.threeBed === "Yes")) &&
+          (filters.fourBed === "All" ||
+            development.availability.fourPlusBed.available ===
+              (filters.fourBed === "Yes"))
+        );
+      });
+      setData(filteredData);
     } catch (error) {
-      setError(error);
+      setErrorMessage(error.message);
     } finally {
       setLoading(false);
     }
@@ -39,39 +74,150 @@ export default function DevelopmentsTable({ data, setData, setLoading }) {
   };
 
   const handleFormSubmit = async (formData) => {
-    console.log(formData);
     try {
-      const result = await createDevelopment(formData);
-      console.log(result);
+      await createDevelopment(formData);
       refreshData();
       setIsModalOpen(false);
       setErrorMessage("");
     } catch (error) {
-      console.error("Error creating development:", error);
       setErrorMessage("Error creating development: " + error.message);
     }
   };
 
+  const handleFilterChange = (e) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className={styles["table-container"]}>
-      <h1>Property Developments</h1>
+      <div className={styles["table-header"]}>
+        <h1>Property Developments</h1>
+      </div>
+
       <table>
         <thead>
           <tr>
             <th>Name</th>
-            <th>Zone</th>
-            <th>Parking</th>
-            <th>1 Bed Availability</th>
+            <th>
+              Zone
+              <div className={styles["filters"]}>
+                <select
+                  id="zone-filter"
+                  name="zone"
+                  value={filters.zone}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  {Array.from({ length: 9 }, (_, i) => i + 1).map((zone) => (
+                    <option key={zone} value={zone}>
+                      {zone}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </th>
+            <th>
+              Parking
+              <div className={styles["filters"]}>
+                <select
+                  id="parking-filter"
+                  name="parking"
+                  value={filters.parking}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+            </th>
+            <th>
+              1 Bed Availability
+              <div className={styles["filters"]}>
+                <select
+                  id="availability-filter"
+                  name="oneBed"
+                  value={filters.oneBed}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+            </th>
             <th>1 Bed Price From</th>
-            <th>2 Bed Availability</th>
+            <th>
+              2 Bed Availability
+              <div className={styles["filters"]}>
+                <select
+                  id="availability-filter"
+                  name="twoBed"
+                  value={filters.twoBed}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+            </th>
             <th>2 Bed Price From</th>
-            <th>3 Bed Availability</th>
+            <th>
+              3 Bed Availability
+              <div className={styles["filters"]}>
+                <select
+                  id="availability-filter"
+                  name="threeBed"
+                  value={filters.threeBed}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+            </th>
             <th>3 Bed Price From</th>
-            <th>4+ Bed Availability</th>
+            <th>
+              4+ Bed Availability
+              <div className={styles["filters"]}>
+                <select
+                  id="availability-filter"
+                  name="fourBed"
+                  value={filters.fourBed}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+            </th>
             <th>4+ Bed Price From</th>
             <th>Postcode</th>
             <th>Developer</th>
-            <th>Cardinal Location</th>
+            <th>
+              Cardinal Location
+              <div className={styles["filters"]}>
+                <select
+                  id="cardinal-filter"
+                  name="cardinal"
+                  value={filters.cardinal}
+                  onChange={handleFilterChange}
+                >
+                  <option value="All">All</option>
+                  <option value="central">Central</option>
+                  <option value="north">North</option>
+                  <option value="east">East</option>
+                  <option value="west">West</option>
+                  <option value="south">South</option>
+                </select>
+              </div>
+            </th>
             <th>Fee</th>
             <th>Contact Email</th>
             <th>Updated At</th>
@@ -147,7 +293,7 @@ export default function DevelopmentsTable({ data, setData, setLoading }) {
             <DevelopmentForm onSubmit={handleFormSubmit} />
             {errorMessage && (
               <div className={styles["error-message"]} style={{ color: "red" }}>
-                Error creating development
+                {errorMessage}
               </div>
             )}
           </div>
