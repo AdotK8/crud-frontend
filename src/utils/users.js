18f9dc0a801bb148f280a3549f027489f509f236
Ahmed <ahmedkhan895.ak@gmail.com>
2024-10-09 22:39:22 +0100

    added users to user.js

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,4 +1,6 @@
 export const users = [
   { Ahmed: "ahmed@yaseproperty.com" },
   { Jason: "jason@yaseproperty.com" },
+  { Gordon: "gordon@yaseproperty.com" },
+  { Derran: "derran@yaseproperty.com" },
 ];
